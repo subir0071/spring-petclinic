cdd90b94046630ba9c165d5b09e23edc0df53f70
Subir Ghosh <techguy007@gmail.com>
2019-02-06 14:06:57 +0530

    testing auto deployment

--- src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/ServletInitializer.java
@@ -6,6 +6,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PetClinicApplication.class);
+		return application.sources(PetClinicApplication.class); 
 	}
 }

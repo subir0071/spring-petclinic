6a70b3528d617802ef6242cc732de6e67b3eccbc
Subir Ghosh <techguy007@gmail.com>
2019-06-10 15:50:01 +0530

    added war type

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,7 +26,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PetClinicApplication.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
